37feb223341ee2f096f17ae674b75166ebeda4bf
Jaz <jasminemaslen7@gmail.com>
2024-11-18 18:54:13 +0000

    finished styling

--- src/App.js
+++ src/App.js
@@ -3,29 +3,33 @@ import "./App.css";
 function App() {
 	return (
 		<div className="App">
-			<form>
-				<input
-					className="input-text"
-					type="text"
-					placeholder="Enter a city here..."
-					required
-				/>
-				<input className="input-submit" type="submit" />
-			</form>
-			<div className="title">
-				<h1>London</h1>
-				<h2>
-					24<span className="degrees">°C</span>
-				</h2>
+			<div className="container">
+				<form>
+					<input
+						className="input-text"
+						type="text"
+						placeholder="Enter a city here..."
+						required
+					/>
+					<input className="input-submit" type="submit" />
+				</form>
+				<div className="title">
+					<div className="title-position">
+						<h1>London</h1>
+						<h2>
+							24<span className="degrees">°C</span>
+						</h2>
+					</div>
+					<div className="date">Thursday 31 October</div>
+				</div>
+				<ul>
+					<li>condition: light rain</li>
+					<li>humidity: 44%</li>
+					<li>wind speed: 15km/h</li>
+				</ul>
 			</div>
-			<ul>
-				<li>Thursday 31 October</li>
-				<li>condition: light rain</li>
-				<li>humidity: 44%</li>
-				<li>wind speed: 15km/h</li>
-			</ul>
 			<div className="footer">
-				Coded by Jaz and open sourced on
+				Coded by Jaz and open sourced on{" "}
 				<a
 					href="https://github.com/Jaz-spec/Weather-app-react"
 					target="_blank"
