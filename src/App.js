2001938811626f27f8d16da97cbd497440ec012e
Jaz <jasminemaslen7@gmail.com>
2024-11-18 15:29:00 +0000

    used CSS to add spacing and partly style

--- src/App.js
+++ src/App.js
@@ -4,18 +4,27 @@ function App() {
 	return (
 		<div className="App">
 			<form>
-				<input type="text" placeholder="Enter a city here..." required />
-				<input type="submit" />
+				<input
+					className="input-text"
+					type="text"
+					placeholder="Enter a city here..."
+					required
+				/>
+				<input className="input-submit" type="submit" />
 			</form>
-			<h1>London</h1>
-			<h2>24 degrees</h2>
+			<div className="title">
+				<h1>London</h1>
+				<h2>
+					24<span className="degrees">°C</span>
+				</h2>
+			</div>
 			<ul>
 				<li>Thursday 31 October</li>
 				<li>condition: light rain</li>
 				<li>humidity: 44%</li>
 				<li>wind speed: 15km/h</li>
 			</ul>
-			<div>
+			<div className="footer">
 				Coded by Jaz and open sourced on
 				<a
 					href="https://github.com/Jaz-spec/Weather-app-react"
